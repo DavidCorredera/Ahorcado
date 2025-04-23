f2dbb2d0232b3a066504e8381a28d2932e653a04
am4DAM1 <davidcorrederacamara@gmail.com>
2025-04-23 10:17:41 +0200

    Starting project

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,62 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Random;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    static String palabra;
+    static String dificultad;
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al ahorcado, selecciona si quieres seleccionar dificultad o una dificultad aleatoria:");
+        System.out.println();
+        System.out.println("1. Seleccionar dificultad.");
+        System.out.println("2. Dificultad aleatoria.");
+        System.out.println();
+
+        int seleccion = sc.nextInt();
+        if (seleccion == 1) {
+            System.out.println("Selecciona dificultad:");
+            System.out.println();
+            System.out.println("1. Fácil.");
+            System.out.println("2. Medio.");
+            System.out.println("3. Difícil.");
+            System.out.println();
+
+            seleccion = sc.nextInt();
+
+            switch (seleccion) {
+                case 1:
+                    dificultad = "F";
+                    break;
+                case 2:
+                    dificultad = "M";
+                    break;
+                case 3:
+                    dificultad = "D";
+                    break;
+            }
+        } else {
+            dificultad = getRandomDiff();
         }
+
+        System.out.println("Dificultad seleccionada: " + getCompleteDiff(dificultad));
+    }
+
+    private static String getRandomDiff() {
+        String[] dificultadArray = {"F", "M", "D"};
+        return dificultadArray[(new Random()).nextInt(dificultadArray.length)]; // devolver un elemento aleatorio del array
+    }
+
+    private static String getCompleteDiff(String diff) {
+        switch (diff) {
+            case "F":
+                return "Fácil";
+            case "M":
+                return "Medio";
+            case "D":
+                return "Difícil";
+        }
+        return diff;
     }
 }
