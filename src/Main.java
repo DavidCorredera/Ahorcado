48d9b624b44387b6b6ccd64458dd7b1b93c95c52
Marcoshervas <hervasmarcos99@gmail.com>
2025-04-23 10:50:56 +0200

    Figuras y bucle

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,42 @@ public class Main {
     static String palabra;
     static String dificultad;
     static Scanner sc = new Scanner(System.in);
-
+    static String[] vidas = {"--------\n" +
+            "|     |\n" +
+            "|    \n" +
+            "|    \n" +
+            "|    \n" +
+            "|_________/n","--------\n" +
+            "|     |\n" +
+            "|     O\n" +
+            "|    \n" +
+            "|    \n" +
+            "|_________","--------\n" +
+            "|     |\n" +
+            "|     O\n" +
+            "|     |\n" +
+            "|    \n" +
+            "|_________","--------\n" +
+            "|     |\n" +
+            "|     O\n" +
+            "|    /|\n" +
+            "|    \n" +
+            "|_________","--------\n" +
+            "|     |\n" +
+            "|     O\n" +
+            "|    /|\\\n" +
+            "|    \n" +
+            "|_________","--------\n" +
+            "|     |\n" +
+            "|     O\n" +
+            "|    /|\\\n" +
+            "|    / \n" +
+            "|_________","--------\n" +
+            "|     |\n" +
+            "|     O\n" +
+            "|    /|\\\n" +
+            "|    / \\\n" +
+            "|_________"};
     public static void main(String[] args) {
         System.out.println("Bienvenido al ahorcado, selecciona si quieres seleccionar dificultad o una dificultad aleatoria:");
         System.out.println();
@@ -16,26 +51,33 @@ public class Main {
 
         int seleccion = sc.nextInt();
         if (seleccion == 1) {
-            System.out.println("Selecciona dificultad:");
-            System.out.println();
-            System.out.println("1. Fácil.");
-            System.out.println("2. Medio.");
-            System.out.println("3. Difícil.");
-            System.out.println();
+            boolean repetir = false;
+            do{
+                System.out.println("Selecciona dificultad:");
+                System.out.println();
+                System.out.println("1. Fácil.");
+                System.out.println("2. Medio.");
+                System.out.println("3. Difícil.");
+                System.out.println();
 
-            seleccion = sc.nextInt();
+                seleccion = sc.nextInt();
 
-            switch (seleccion) {
-                case 1:
-                    dificultad = "F";
-                    break;
-                case 2:
-                    dificultad = "M";
-                    break;
-                case 3:
-                    dificultad = "D";
-                    break;
-            }
+                switch (seleccion) {
+                    case 1:
+                        dificultad = "F";
+                        break;
+                    case 2:
+                        dificultad = "M";
+                        break;
+                    case 3:
+                        dificultad = "D";
+                        break;
+                    default:
+                        repetir = true;
+                        System.out.println("Introduce un valor correcto.");
+                }
+            }while(repetir);
+
         } else {
             dificultad = getRandomDiff();
         }
